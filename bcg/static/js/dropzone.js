d109058c8d6229196a7d1f180655238fc0ba30dc
Erasmus Hagen <erasmus@degeist.com>
2016-07-26 17:28:58 +0200

    Update for test app

--- bcg/static/js/dropzone.js
+++ bcg/static/js/dropzone.js
@@ -1,27 +1,9 @@
-/*jslint unparam: true */
-/*global window, $ */
-// $(function () {
-//     'use strict';
-//     // Change this to the location of your server-side upload handler:
-//     var url = 'server/php/';
-//     $('#fileupload').fileupload({
-//         url: url,
-//         dataType: 'json',
-//         done: function (e, data) {
-//             $.each(data.result.files, function (index, file) {
-//                 $('<p/>').text(file.name).appendTo('#files');
-//             });
-//         },
-//         progressall: function (e, data) {
-//             var progress = parseInt(data.loaded / data.total * 100, 10);
-//             $('#progress .progress-bar').css(
-//                 'width',
-//                 progress + '%'
-//             );
-//         }
-//     }).prop('disabled', !$.support.fileInput)
-//         .parent().addClass($.support.fileInput ? undefined : 'disabled');
-// });
+function prettydump(obj) {
+  $.each(obj, function(key, value) {
+    console.log("Key: " + key + " Value: " + value);
+  });
+}
+
 
 $(function () {
   $('#direct_upload input[type="file"]')
@@ -54,6 +36,8 @@ $(function () {
       $(background).css("background-image", "url(" + uploadedBgImg +")");
 
       $('#backgroundUploadModal').modal('hide');
+
+          prettydump(data.result);
 
   });
 });
